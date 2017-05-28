033089fa023b2288c141405df88642be208349e2
ccbetter <weibetter@qq.com>
2017-05-28 20:04:45 +0800

    init

---
+++ ccbeta-service/ccbeta-product-service/src/main/java/com/ccbeta/cloud/product/service/App.java
@@ -0,0 +1,13 @@
+package com.ccbeta.cloud.product.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
